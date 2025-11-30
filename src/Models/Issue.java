4d703d451f652e314c14353c46a365480f555f8e
mkasem06 <m.kasem2506@gmail.com>
2025-11-30 20:39:24 +0200

    Refactor Issue class to use a map for duplicate value indices and improve data encapsulation

--- src/Models/Issue.java
+++ src/Models/Issue.java
@@ -1,38 +1,48 @@
 package Models;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Issue {
 
     private final IssueType type; // ROW, COL, BOX
-    private final int index; // 1 to 9
-    private final int duplicatedValue; // The repeated digit
-    private final int[] fullArray; // The full row/col/box values
+    private final int locationIndex; // The row/col/box number (1-9)
+    private final Map<Integer, List<Integer>> indicesMap; // Maps duplicate value to list of indices where it appears
     //all the variables are final because they should not change after creation
 
-    public Issue(IssueType type, int index, int duplicatedValue, int[] fullArray) {
+    public Issue(IssueType type, int locationIndex, int duplicatedValue, List<Integer> duplicateIndices) {
         this.type = type;
-        this.index = index;
-        this.duplicatedValue = duplicatedValue;
-        this.fullArray = Arrays.copyOf(fullArray, fullArray.length);
+        this.locationIndex = locationIndex;
+        this.indicesMap = new HashMap<>();
+
+        // Store a defensive copy of the list
+        this.indicesMap.put(duplicatedValue, new ArrayList<>(duplicateIndices));
     }
 
     public IssueType getType() {
         return type;
     }
 
-    public int getIndex() {
-        return index;
+    public int getLocationIndex() {
+        return locationIndex;
     }
 
-    public int getDuplicatedValue() {
-        return duplicatedValue;
+    public Map<Integer, List<Integer>> getIndicesMap() {
+        // Return a defensive copy
+        Map<Integer, List<Integer>> copy = new HashMap<>();
+        for (Map.Entry<Integer, List<Integer>> entry : indicesMap.entrySet()) {
+            copy.put(entry.getKey(), new ArrayList<>(entry.getValue()));
+        }
+        return copy;
     }
 
-    public int[] getFullArray(){
-        return Arrays.copyOf(fullArray, fullArray.length);
-    }
     @Override
     public String toString() {
-        return type + " " + index + ", #" + duplicatedValue + ", " + Arrays.toString(fullArray);
+        // Get the first (and only) entry from the map
+        Map.Entry<Integer, List<Integer>> entry = indicesMap.entrySet().iterator().next();
+        int duplicatedValue = entry.getKey();
+        List<Integer> duplicateIndices = entry.getValue();
+        return type + " " + locationIndex + ", #" + duplicatedValue + ", " + duplicateIndices;
     }
 }
